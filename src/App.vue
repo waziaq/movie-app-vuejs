aff205e62df8276ea4ae5574a85ffb0223d3a49a
waziaq <mr.zexr2010@gmail.com>
2020-03-01 03:46:29 +0800

    Update toolbars and logo

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,15 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <v-toolbar app flat color="grey">
+      <v-container grid-list-xl>
+        <router-link to='/' tag='span' style='cursor: pointer'>
+          <img class="mr-3" src="@/assets/wmovie.png" height="90"/>
+        </router-link>
+        <v-spacer></v-spacer>
+        <div class="float-right">
+        </div>
+      </v-container>
+    </v-toolbar>
     <v-content>
       <router-view></router-view>
     </v-content>
